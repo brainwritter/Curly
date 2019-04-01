f4ffcee10fa00b5bc7c96c2d47609a9ddab39bb3
bobbyecho <raden.bambang@ralali.com>
2019-04-01 11:18:53 +0700

    save

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App/Setup/App';
+import { name as appName } from './app.json';
 // import './App/Setup/Reactotron';
 
-AppRegistry.registerComponent('curly', () => App);
+AppRegistry.registerComponent(appName, () => App);
